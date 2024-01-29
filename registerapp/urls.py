7f6c6b269e27391d7f46edfbbf94fcfc295c5c91
Felipe Torres <ftorres@intralog.local>
2024-01-29 16:57:19 -0300

    estamos ya casi con login y logout, pero no se por que no renderizan los CSS

--- registerapp/urls.py
+++ registerapp/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path, include
 from .views import *
+from django.contrib.auth.views import LogoutView
 
 urlpatterns = [
-    path('',login1, name='login')
+    path('',login1, name='login'),
+    path('logout/', LogoutView.as_view(template_name='logout.html'), name="logout")
 ]
