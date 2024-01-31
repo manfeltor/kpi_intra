4c737fe7366c5d114cfc28c4944b98f028eba341
Felipe Torres <ftorres@intralog.local>
2024-01-30 21:12:13 -0300

    hay q migrar el modelo que estamos haciendo, pero segui mejor la conversacion con GPT, ahi esta mas clara la intencion

--- entregasapp/views.py
+++ entregasapp/views.py
@@ -1,7 +1,21 @@
 from django.shortcuts import render
+import pandas as pd
+import os
 
 # Create your views here.
+
+# render
 
 def render_main(req):
 
     return render(req, "entregasmain.html")
+
+#db
+
+def importar_excel_tms() -> pd.DataFrame:
+
+    folder_path = ''
+
+    file_names = os.listdir(folder_path)
+    
+    return baselimpia
