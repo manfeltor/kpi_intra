7dee4b8fb65f8df85b1b87b9c4e07811157f6d31
felipe <manfeltor@live.com>
2024-02-06 17:51:53 -0300

    estamos con lo mas dificil que era limpiar las funciones, ahora a calcular y renderizar

--- testing.py
+++
@@ -1,9 +0,0 @@
-from entregasapp.views import importar_excel_tms
-import pandas as pd
-
-dtp = importar_excel_tms(r'C:\Users\ftorres\OneDrive - INTRALOG ARGENTINA S.A\kpi\dash_pr\TMS_por_meses\*.xlsx').dtypes
-
-with pd.option_context('display.max_rows', None, 'display.max_columns', None):
-    print(dtp)
-
-    
