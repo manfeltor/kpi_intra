d3448306e425d0009e26963eb0aad47582defd3c
felipe <manfeltor@live.com>
2024-03-05 17:50:01 -0300

    forms and classes are done, we need to render and recieve

--- entregasapp/forms.py
+++ entregasapp/forms.py
@@ -5,4 +5,19 @@ class DateRangeForm(forms.Form):
     end_date = forms.DateField(label='until_date', widget=forms.DateInput(attrs={'type': 'date'}))
 
 class DeliveryTypesForm(forms.Form):
-    filter_option1 = forms.BooleanField(required=False)
+    AMBA = forms.BooleanField(required=False)
+    INTERIOR = forms.BooleanField(required=False)
+
+class ZonaForm(forms.Form):
+    AMBA = forms.BooleanField(required=False)
+    INTERIOR = forms.BooleanField(required=False)
+
+    def clean(self):
+        cleaned_data = super().clean()
+        AMBA = cleaned_data.get('filter_option1', False)
+        INTERIOR = cleaned_data.get('filter_option2', False)
+
+        if AMBA and INTERIOR:
+            raise forms.ValidationError("Only one option can be selected")
+
+        return cleaned_data
