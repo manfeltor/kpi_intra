b5ad34d83d3a95786654a5e3eb30b5fee495212a
felipe <manfeltor@live.com>
2024-03-05 16:00:50 -0300

    we done the classes for the funcs filtering issue, now we are mettling w the views

--- entregasapp/forms.py
+++ entregasapp/forms.py
@@ -1,5 +1,8 @@
 from django import forms
 
 class DateRangeForm(forms.Form):
-    start_date = forms.DateField(label='Start Date', widget=forms.DateInput(attrs={'type': 'date'}))
-    end_date = forms.DateField(label='End Date', widget=forms.DateInput(attrs={'type': 'date'}))
+    start_date = forms.DateField(label='from_date', widget=forms.DateInput(attrs={'type': 'date'}))
+    end_date = forms.DateField(label='until_date', widget=forms.DateInput(attrs={'type': 'date'}))
+
+class DeliveryTypesForm(forms.Form):
+    filter_option1 = forms.BooleanField(required=False)
