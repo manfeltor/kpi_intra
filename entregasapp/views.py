df9f685d670200220abb2506096a32b61d1a93c9
Felipe Torres <ftorres@intralog.local>
2024-05-03 15:24:10 -0300

    seteado el git large file storage y reparado el manager de base de datos de OMS

--- entregasapp/views.py
+++ entregasapp/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 import pandas as pd
 import numpy as np
-from .models import bdoms
+from .models import bdoms, cpPais
 from django.db.models import Q
 import numpy as np
 from .forms import DateRangeForm, DeliveryTypesForm
@@ -184,3 +184,44 @@ def mean_group_date_diff(df, grcol, datecol1, datecol2=None):
     mean_per_group.sort_values(by=datecol1, inplace=True)
     
     return mean_per_group
+
+def importar_excel_tms(folder_path) -> pd.DataFrame:
+    df = pd.read_excel(folder_path)
+    df = df[[
+    'pedido',
+    'flujo',
+    'seller',
+    'sucCodigo',
+    'estadoPedido',
+    'fechaCreacion',
+    'fechaRecepcion',
+    'fechaDespacho',
+    'fechaEntrega',
+    'lpn',
+    'estadoLpn',
+    'trackingDistribucion',
+    'trackingTransporte',
+    'tipo',
+    'codigoPostal',
+    'tte',
+    'tteSucursalDistribucion',
+    'tiendaEntrega',
+    'zona'
+    ]]
+    date_columns = ['fechaCreacion', 'fechaRecepcion', 'fechaDespacho', 'fechaEntrega']
+    for column in date_columns:
+        df[column] = pd.to_datetime(df[column])
+    df['codigoPostal'] = df['codigoPostal'].astype(object)
+    for index, row in df.iterrows():
+        cp_value = row['codigoPostal']  # Assuming this is the CP value as a string
+        cp_instance = cpPais.objects.get(CP=cp_value)
+        df.at[index, 'codigoPostal'] = cp_instance
+    bdfin = df
+    for column in date_columns:
+        print(column)
+        bdfin[column] = pd.to_datetime(bdfin[column])
+    bdfin.replace({pd.NaT: None, np.nan: None}, inplace=True)
+    bdfin.replace('nan', None)
+    # bdfin = bdfin.rename(columns=lambda x: x.replace('.', 'x'))
+    # print(type(bdfin))
+    return bdfin
